1a20514c3377ad43031f070e2981a99afd2e1784
martinjensen37 <martinjensen37@gmail.com>
2025-06-23 13:18:21 +0200

    Updated the MQTTLooter application

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+ipcMain.on('message', (event, message) => {
+    // Handle incoming messages from the renderer process
+});
